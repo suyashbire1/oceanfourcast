6d848231a1ade965de6230036f4d907cee485b3f
Suyash Bire <bire@mit.edu>
2023-01-10 12:44:19 -0500

    Passes device to Unet

--- oceanfourcast/unet.py
+++ oceanfourcast/unet.py
@@ -5,21 +5,33 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class DoubleConv(nn.Module):
     """(convolution => [BN] => ReLU) * 2"""
 
-    def __init__(self, in_channels, out_channels, mid_channels=None):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 mid_channels=None,
+                 device='cpu'):
         super().__init__()
         if not mid_channels:
             mid_channels = out_channels
         self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(mid_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
-        )
+            nn.Conv2d(in_channels,
+                      mid_channels,
+                      kernel_size=3,
+                      padding=1,
+                      bias=False,
+                      device=device),
+            nn.BatchNorm2d(mid_channels, device=device), nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels,
+                      out_channels,
+                      kernel_size=3,
+                      padding=1,
+                      bias=False,
+                      device=device),
+            nn.BatchNorm2d(out_channels, device=device), nn.ReLU(inplace=True))
 
     def forward(self, x):
         return self.double_conv(x)
@@ -28,12 +40,11 @@ class DoubleConv(nn.Module):
 class Down(nn.Module):
     """Downscaling with maxpool then double conv"""
 
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, device='cpu'):
         super().__init__()
         self.maxpool_conv = nn.Sequential(
             nn.MaxPool2d(2),
-            DoubleConv(in_channels, out_channels)
-        )
+            DoubleConv(in_channels, out_channels, device=device))
 
     def forward(self, x):
         return self.maxpool_conv(x)
@@ -42,16 +53,25 @@ class Down(nn.Module):
 class Up(nn.Module):
     """Upscaling then double conv"""
 
-    def __init__(self, in_channels, out_channels, bilinear=True):
+    def __init__(self, in_channels, out_channels, bilinear=True, device='cpu'):
         super().__init__()
 
         # if bilinear, use the normal convolutions to reduce the number of channels
         if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+            self.up = nn.Upsample(scale_factor=2,
+                                  mode='bilinear',
+                                  align_corners=True)
+            self.conv = DoubleConv(in_channels,
+                                   out_channels,
+                                   in_channels // 2,
+                                   device=device)
         else:
-            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-            self.conv = DoubleConv(in_channels, out_channels)
+            self.up = nn.ConvTranspose2d(in_channels,
+                                         in_channels // 2,
+                                         kernel_size=2,
+                                         stride=2,
+                                         device=device)
+            self.conv = DoubleConv(in_channels, out_channels, device=device)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
@@ -59,8 +79,9 @@ class Up(nn.Module):
         diffY = x2.size()[2] - x1.size()[2]
         diffX = x2.size()[3] - x1.size()[3]
 
-        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
-                        diffY // 2, diffY - diffY // 2])
+        x1 = F.pad(
+            x1,
+            [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
         # if you have padding issues, see
         # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
         # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
@@ -69,32 +90,38 @@ class Up(nn.Module):
 
 
 class OutConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
+
+    def __init__(self, in_channels, out_channels, device='cpu'):
         super(OutConv, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+        self.conv = nn.Conv2d(in_channels,
+                              out_channels,
+                              kernel_size=1,
+                              device=device)
 
     def forward(self, x):
         return self.conv(x)
 
 
 class UNet(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=False):
+
+    def __init__(self, n_channels, n_classes, bilinear=False, device='cpu'):
         super(UNet, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
+        self.Co = n_classes
         self.bilinear = bilinear
 
-        self.inc = DoubleConv(n_channels, 64)
-        self.down1 = Down(64, 128)
-        self.down2 = Down(128, 256)
-        self.down3 = Down(256, 512)
+        self.inc = DoubleConv(n_channels, 64, device=device)
+        self.down1 = Down(64, 128, device=device)
+        self.down2 = Down(128, 256, device=device)
+        self.down3 = Down(256, 512, device=device)
         factor = 2 if bilinear else 1
-        self.down4 = Down(512, 1024 // factor)
-        self.up1 = Up(1024, 512 // factor, bilinear)
-        self.up2 = Up(512, 256 // factor, bilinear)
-        self.up3 = Up(256, 128 // factor, bilinear)
-        self.up4 = Up(128, 64, bilinear)
-        self.outc = OutConv(64, n_classes)
+        self.down4 = Down(512, 1024 // factor, device=device)
+        self.up1 = Up(1024, 512 // factor, bilinear, device=device)
+        self.up2 = Up(512, 256 // factor, bilinear, device=device)
+        self.up3 = Up(256, 128 // factor, bilinear, device=device)
+        self.up4 = Up(128, 64, bilinear, device=device)
+        self.outc = OutConv(64, n_classes, device=device)
 
     def forward(self, x):
         """
@@ -102,14 +129,14 @@ class UNet(nn.Module):
             x torch.Tensor([batch_size, in_channels, h, w])
         """
         # 2, 9, 248, 248
-        x1 = self.inc(x) # 2, 64, 248, 248 
-        x2 = self.down1(x1) # 2, 128, 124, 124
-        x3 = self.down2(x2) # 2, 256, 62, 62
-        x4 = self.down3(x3) # 2, 512, 31, 31
-        x5 = self.down4(x4) # 2, 1024, 15, 15
-        x = self.up1(x5, x4) # 2, 512, 31, 31
-        x = self.up2(x, x3) # 2, 256, 62, 62
-        x = self.up3(x, x2) # 2, 128, 124, 124
-        x = self.up4(x, x1) # 2, 64, 248, 248
-        logits = self.outc(x) # 2, 9, 248, 248
+        x1 = self.inc(x)  # 2, 64, 248, 248
+        x2 = self.down1(x1)  # 2, 128, 124, 124
+        x3 = self.down2(x2)  # 2, 256, 62, 62
+        x4 = self.down3(x3)  # 2, 512, 31, 31
+        x5 = self.down4(x4)  # 2, 1024, 15, 15
+        x = self.up1(x5, x4)  # 2, 512, 31, 31
+        x = self.up2(x, x3)  # 2, 256, 62, 62
+        x = self.up3(x, x2)  # 2, 128, 124, 124
+        x = self.up4(x, x1)  # 2, 64, 248, 248
+        logits = self.outc(x)  # 2, 9, 248, 248
         return logits
