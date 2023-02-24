8a4d4441af888551d1daf55af4a66ceafeb8598a
Suyash Bire <suyash309@gmail.com>
2023-02-24 17:14:31 -0500

    Setting up wind only experiments

--- oceanfourcast/load_numpy.py
+++ oceanfourcast/load_numpy.py
@@ -8,6 +8,7 @@ from torchvision import transforms
 import click
 import glob
 
+
 @click.command()
 @click.option("--xarray_data_file", default="./dynDiag.nc")
 def save_numpy_file_from_xarray(xarray_data_file):
@@ -15,19 +16,21 @@ def save_numpy_file_from_xarray(xarray_data_file):
     print("Loading data...")
     with xr.open_dataset(xarray_data_file, decode_times=False) as ds:
         channels = [
-            ds.UVEL.isel(Zmd000015=0),    # usurf
-            ds.UVEL.isel(Zmd000015=7),    # umid
-            ds.VVEL.isel(Zmd000015=0),    # vsurf
-            ds.VVEL.isel(Zmd000015=7),    # vmid
-            ds.THETA.isel(Zmd000015=0),   # Tsurf
-            ds.THETA.isel(Zmd000015=7),   # Tmid
+            ds.UVEL.isel(Zmd000015=0),  # usurf
+            ds.UVEL.isel(Zmd000015=7),  # umid
+            ds.VVEL.isel(Zmd000015=0),  # vsurf
+            ds.VVEL.isel(Zmd000015=7),  # vmid
+            ds.THETA.isel(Zmd000015=0),  # Tsurf
+            ds.THETA.isel(Zmd000015=7),  # Tmid
             ds.PHIHYD.isel(Zmd000015=0),  # Psurf
             ds.PHIHYD.isel(Zmd000015=7),  # Pmid
-            ds.PHIHYD.isel(Zmd000015=-1), # Pbot
-            ds.PsiVEL.sum('Zmd000015')    # Psi
+            ds.PHIHYD.isel(Zmd000015=-1),  # Pbot
+            ds.PsiVEL.sum('Zmd000015')  # Psi
         ]
 
-        data = [channel.values.squeeze()[np.newaxis,...] for channel in channels]
+        data = [
+            channel.values.squeeze()[np.newaxis, ...] for channel in channels
+        ]
         data[0] = u_corner_to_center(data[0])
         data[1] = u_corner_to_center(data[1])
         data[2] = v_corner_to_center(data[2])
@@ -36,8 +39,8 @@ def save_numpy_file_from_xarray(xarray_data_file):
 
         nt = np.shape(data[0])[1]
         tau, Trest = create_forcing_arrays(xarray_data_file)
-        tau = np.repeat(tau[np.newaxis, np.newaxis,:,:], nt, axis=1)
-        Trest = np.repeat(Trest[np.newaxis, np.newaxis,:,:], nt, axis=1)
+        tau = np.repeat(tau[np.newaxis, np.newaxis, :, :], nt, axis=1)
+        Trest = np.repeat(Trest[np.newaxis, np.newaxis, :, :], nt, axis=1)
         data.append(tau)
         data.append(Trest)
 
@@ -45,8 +48,8 @@ def save_numpy_file_from_xarray(xarray_data_file):
         data = np.moveaxis(data, 0, 1)
 
         print("Calculating stats...")
-        means = np.mean(data, axis=(0,2,3))
-        stdevs = np.std(data, axis=(0,2,3))
+        means = np.mean(data, axis=(0, 2, 3))
+        stdevs = np.std(data, axis=(0, 2, 3))
         timemeans = np.mean(data, axis=0)
         timestdevs = np.std(data, axis=0)
 
@@ -57,7 +60,11 @@ def save_numpy_file_from_xarray(xarray_data_file):
         numpy_data_file = os.path.join(data_dir, "dynDiags.npy")
         np.save(numpy_data_file, data)
         numpy_stats_file = os.path.join(data_dir, "dynDiagsStats.npz")
-        np.savez(numpy_stats_file, means=means, stdevs=stdevs, timemeans=timemeans, timestdevs=timestdevs)
+        np.savez(numpy_stats_file,
+                 means=means,
+                 stdevs=stdevs,
+                 timemeans=timemeans,
+                 timestdevs=timestdevs)
 
 
 @click.command()
@@ -69,18 +76,18 @@ def save_global_stats(data_rootdir):
     print(f"Working on {f}")
     data = np.load(f, mmap_mode='r')
     nt = data.shape[0]
-    global_means = nt*np.mean(data, axis=(0,2,3))
-    global_stdevs = nt*np.std(data, axis=(0,2,3))
+    global_means = nt * np.mean(data, axis=(0, 2, 3))
+    global_stdevs = nt * np.std(data, axis=(0, 2, 3))
     global_time_steps = nt
 
     for f in files:
         print(f"Working on {f}")
         data = np.load(f, mmap_mode='r')
         nt = data.shape[0]
-        sim_means = np.mean(data, axis=(0,2,3))
-        sim_stdevs = np.std(data, axis=(0,2,3))
-        global_means += nt*sim_means
-        global_stdevs += nt*sim_stdevs
+        sim_means = np.mean(data, axis=(0, 2, 3))
+        sim_stdevs = np.std(data, axis=(0, 2, 3))
+        global_means += nt * sim_means
+        global_stdevs += nt * sim_stdevs
         global_time_steps += nt
 
     global_means /= global_time_steps
@@ -97,6 +104,53 @@ def save_global_stats(data_rootdir):
         np.savez(global_stats_file, means=global_means, stdevs=global_stdevs)
 
 
+@click.command()
+@click.option("--data_rootdir", default="./")
+def save_global_stats_wind_only(data_rootdir):
+    file_pattern = os.path.join(data_rootdir, "**", "dynDiags.npy")
+    files = glob.iglob(file_pattern, recursive=True)
+    f = next(files)
+    print(f"Working on {f}")
+    data = np.load(f, mmap_mode='r')
+    nt = data.shape[0]
+    global_means = nt * np.mean(data, axis=(0, 2, 3))
+    global_stdevs = nt * np.std(data, axis=(0, 2, 3))
+    global_timemeans = nt * np.mean(data, axis=0)
+    global_timestdevs = nt * np.std(data, axis=0)
+    global_time_steps = nt
+
+    for f in files:
+        print(f"Working on {f}")
+        data = np.load(f, mmap_mode='r')
+        nt = data.shape[0]
+        sim_means = np.mean(data, axis=(0, 2, 3))
+        sim_stdevs = np.std(data, axis=(0, 2, 3))
+        global_means += nt * sim_means
+        global_stdevs += nt * sim_stdevs
+        sim_timemeans = nt * np.mean(data, axis=0)
+        sim_timestdevs = nt * np.std(data, axis=0)
+        global_timemeans += nt * sim_timemeans
+        global_timestdevs += nt * sim_timestdevs
+        global_time_steps += nt
+
+    global_means /= global_time_steps
+    global_stdevs /= global_time_steps
+    global_timemeans /= global_time_steps
+    global_timestdevs /= global_time_steps
+
+    print("Saving data...")
+    file_pattern = os.path.join(data_rootdir, "**", "dynDiags.npy")
+    files = glob.iglob(file_pattern, recursive=True)
+    for f in files:
+        data_dir = os.path.dirname(f)
+        global_stats_file = os.path.join(data_dir, "dynDiagsGlobalStats2D.npz")
+        np.savez(global_stats_file,
+                 means=global_means,
+                 stdevs=global_stdevs,
+                 timemeans=global_timemeans,
+                 timestdevs=global_timestdevs)
+
+
 def create_forcing_arrays(xarray_data_file):
     with xr.open_dataset(xarray_data_file, decode_times=False) as ds:
         x = ds.X.values
@@ -111,25 +165,35 @@ def create_forcing_arrays(xarray_data_file):
     nx, ny = x.size, y.size
     xo, yo = x[0], y[0]
     dx, dy = np.diff(x)[0], np.diff(y)[0]
-    tau = -taumax * np.cos(2*np.pi*((y-yo)/(ny-2)/dy))
+    tau = -taumax * np.cos(2 * np.pi * ((y - yo) / (ny - 2) / dy))
     tau = np.repeat(tau[:, np.newaxis], x.size, axis=1)
-    Trest = (Tmax-Tmin)/(ny-2)/dy * (yo-y) + Tmax
+    Trest = (Tmax - Tmin) / (ny - 2) / dy * (yo - y) + Tmax
     Trest = np.repeat(Trest[:, np.newaxis], x.size, axis=1)
     return tau, Trest
 
+
 def u_corner_to_center(u):
-    return (u[...,:-1] + u[...,1:])/2
+    return (u[..., :-1] + u[..., 1:]) / 2
+
 
 def v_corner_to_center(v):
-    return (v[...,:-1,:] + v[...,1:,:])/2
+    return (v[..., :-1, :] + v[..., 1:, :]) / 2
+
 
 if __name__ == "__main__":
     #save_numpy_file_from_xarray()
-    save_global_stats()
+    save_global_stats_wind_only()
 
 
 class OceanDataset(Dataset):
-    def __init__(self, data_file, tslag=3, spinupts=0, fine_tune=False, device='cpu', multi_expt_normalize=False):
+
+    def __init__(self,
+                 data_file,
+                 tslag=3,
+                 spinupts=0,
+                 fine_tune=False,
+                 device='cpu',
+                 multi_expt_normalize=False):
         self.tslag = tslag
         self.spinupts = spinupts
 
@@ -141,20 +205,22 @@ class OceanDataset(Dataset):
 
         self.data = np.load(data_file, mmap_mode=mmap_mode)[spinupts:]
         if multi_expt_normalize is True:
-            stats_file = np.load(os.path.join(data_dir, "dynDiagsGlobalStats.npz"))
+            stats_file = np.load(
+                os.path.join(data_dir, "dynDiagsGlobalStats2D.npz"))
         else:
             stats_file = np.load(os.path.join(data_dir, "dynDiagsStats.npz"))
         self.means = stats_file['means']
         self.stdevs = stats_file['stdevs']
 
         self.transform = transforms.Normalize(mean=self.means, std=self.stdevs)
-        self.target_transform = transforms.Normalize(mean=self.means, std=self.stdevs)
+        self.target_transform = transforms.Normalize(mean=self.means,
+                                                     std=self.stdevs)
 
         self.img_size = [self.data.shape[-1], self.data.shape[-2]]
         self.channels = self.data.shape[1]
         self.fine_tune = fine_tune
         if self.fine_tune:
-            self.len_ = self.data.shape[0] - self.tslag*2
+            self.len_ = self.data.shape[0] - self.tslag * 2
             self.getitem = self.getitem_finetune
         else:
             self.len_ = self.data.shape[0] - self.tslag
@@ -173,7 +239,8 @@ class OceanDataset(Dataset):
             label torch.Tensor([channels, h, w])
         """
         data = self.transform(torch.tensor(self.data[idx]))
-        label = self.target_transform(torch.tensor(self.data[idx+self.tslag]))
+        label = self.target_transform(torch.tensor(self.data[idx +
+                                                             self.tslag]))
         return data, label
 
     def getitem_finetune(self, idx):
@@ -183,5 +250,7 @@ class OceanDataset(Dataset):
             label [torch.Tensor([channels, h, w]), torch.Tensor([channels, h, w])]
         """
         data = self.transform(torch.tensor(self.data[idx]))
-        label = self.target_transform(torch.tensor(self.data[idx+self.tslag])), self.target_transform(torch.tensor(self.data[idx+2*self.tslag]))
+        label = self.target_transform(torch.tensor(
+            self.data[idx + self.tslag])), self.target_transform(
+                torch.tensor(self.data[idx + 2 * self.tslag]))
         return data, label
